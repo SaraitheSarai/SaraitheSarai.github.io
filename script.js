8b9a87185eca0a01e736c29a420bbf384e546d5b
SaraitheSarai <windlewolf506@gmail.com>
2024-07-31 14:58:53 -0500

    18th commit

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-let randomnumber = math.floor(math.random() * 100) + 1;
+let randomnumber = Math.floor(Math.random() * 100) + 1;
 
 function makeGuess() {
   const userGuess =  document.getElementById('guess input').Value;
   const message = document.getElementById('message');
-if(userGuess ==randomnumber){
-    message.textContent = "congrats! you guess the number.";
-    message.style.color = 'bright pink';
-    resetGame();
-}else if (userGuess > randomnumber) {
-   message.textContent = 'to high! try again.';
-   message.style.color = 'red';
-}else if (userGuess > randomnumber){
-    message.textContent = 'to low! try again.';
-    message.style.color = 'red';
-  }
+    if(userGuess ==randomnumber){
+        message.textContent = "congrats! you guess the number";
+        message.style.color = 'pink';
+        resetGame();
+    }else if (userGuess > randomnumber) {
+        message.textContent = 'to high! try again';
+        message.style.color = 'red';
+    }else if (userGuess > randomnumber){
+        message.textContent = 'to low! try again.';
+        message.style.color = 'red';
+    }
 }
-function.resetGame(){
-    randomnumber = Math.floor(math.random() * 100) + 1;
+function resetGame(){
+    randomnumber = Math.floor(Math.random() * 100) + 1;
     document.getElementById('guess-input').value ='';
 }
 
